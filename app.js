0c8831638ad899b4cf0c4e7a4593cd9ce2fe1ecf
Maribel <minicatsCB@users.noreply.github.com>
2019-06-20 00:10:03 +0200

    Search for related movies of the selected one

--- app.js
+++ app.js
@@ -13,9 +13,28 @@ puppeteer.launch({ headless: true }).then(async browser => {
         const page = await browser.newPage();
         await page.goto(`https://www.imdb.com/find?q=${answer.movieTitle}&s=tt&exact=true&ref_=fn_al_tt_ex`);
         let data = await getFilmsFromTable(page);
-        console.log("Obtained data", data);
-        await browser.close();
+
+        inquirer.prompt([
+            {
+                type: 'list',
+                name: 'relatedTo',
+                message: 'Select the movie you want to search related movies of:',
+                choices: data.map(movie => {
+                    return {
+                        name: movie.text,
+                        value: movie.link
+                    };
+                })
+            }
+        ]).then(async answer => {
+            console.log("Searching related movies...");
+            await page.goto(answer.relatedTo);
+            let data = await getRelatedMovies(page);
+            console.log("Related movies:", data.map(movie => movie.title));
+            await browser.close();
+        });
     });
+
 });
 
 async function getFilmsFromTable(page) {
@@ -30,6 +49,24 @@ async function getFilmsFromTable(page) {
                 "title": movie.querySelector(".result_text > a").textContent,
                 "text": movie.textContent.trim(),
                 "link": link,
+                "id": id
+            });
+        });
+
+        return data;
+    });
+}
+
+async function getRelatedMovies(page){
+    return result = await page.evaluate(() => {
+        const movies = document.querySelectorAll(".rec_item");
+
+        let data = [];
+        movies.forEach(movie => {
+            let id = movie.dataset.tconst;
+            data.push({
+                "title": movie.querySelector("a > img").title,
+                "link": `https://www.imdb.com/title/${id}/?ref_=tt_rec_tti`,
                 "id": id
             });
         });
